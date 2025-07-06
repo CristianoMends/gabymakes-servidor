a35f082062967f071cad9b695cac824ce47fcf5a
CristianoMends <mendescristiano012@gmail.com>
2025-07-06 10:23:10 -0300

    modificado database url e add vercel.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,11 +16,7 @@ import { AuthModule } from './auth/auth.module';
     UploadModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432'),
-      username: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
+      url: process.env.DATABASE_URL,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
