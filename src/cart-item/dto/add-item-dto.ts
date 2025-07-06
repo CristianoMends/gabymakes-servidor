57767e77c02f717f1579d231d88204bcd3cf878f
CristianoMends <mendescristiano012@gmail.com>
2025-07-06 16:16:04 -0300

    add cart

--- src/cart-item/dto/add-item-dto.ts
+++ src/cart-item/dto/add-item-dto.ts
@@ -1,5 +1,13 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class AddItemDto {
+    @IsString()
+    @IsNotEmpty()
     userId: string;
+    @IsString()
+    @IsNotEmpty()
     productId: string;
+    @IsString()
+    @IsNotEmpty()
     quantity: number;
 }
